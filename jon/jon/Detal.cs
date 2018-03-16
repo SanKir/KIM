a8f6355d4e7d1157afa472b79da7f278dcb760db
ButovIvan <Super_address1998@outlook.com>
2018-03-16 19:05:40 +0700

    kjdb

---
+++ jon/jon/Detal.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jon
+{
+    class Detal
+    {
+        int number;
+        int stanok1;
+        int stanok2;
+        public Detal()
+        {
+            number = 1;
+            stanok1 = 0;
+            stanok2 = 4;
+        }
+        public Detal(int number, int stanok1, int stanok2)
+        {
+            this.number = number;
+            this.stanok1 = stanok1;
+            this.stanok2 = stanok2;
+        }
+        public int GetNumber()
+        {
+            return number;
+        }
+        public int GetStanok1()
+        {
+            return stanok1;
+        }
+        public int GetStanok2()
+        {
+            return stanok2;
+        }
+    }
+}
---
+++ jon/jon/NameComparer2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jon
+{
+    class NameComparer2 : IComparer<Detal>
+    {
+        public int Compare(Detal o1, Detal o2)
+        {
+            if (o1.GetStanok1() > o2.GetStanok1())
+            {
+                return 1;
+            }
+            else if (o1.GetStanok1() < o2.GetStanok1())
+            {
+                return -1;
+            }
+
+            return 0;
+        }
+    }
+}
