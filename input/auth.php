be67f6ab837fbaaf163d30bdce3749bf847c12a4
ButovIvan <Super_address1998@outlook.com>
2018-03-17 23:37:16 +0700

    ff

---
+++ input/auth.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<link rel="stylesheet" type="text/css" href="../style1.css">
+	<meta charset="UTF-8">
+	<title>Main</title>
+</head>
+<body>
+	<div class="wrapper">
+
+		<?php include "../includes/header.php"; ?>
+
+		<div class="main_content clearfix">
+		<div class="poleAuth"><h1></h1></div>
+			<div class="contentAuth">
+				<h1 id="h1Auth">Авторизация</h1>
+				<input type="login" name="mail" size="20" placeholder="Эл.почта: Ivanov@mail.ru" class="input2"></input><Br>
+				
+				<input type="login" name="surname" size="20" placeholder="Пароль: ********" class="input2"></input><Br>
+				
+				<input type="submit" name="reg" size="20" value="Вход" id="button2"></input><Br>
+				
+				
+			</div>
+			<div class="sidebarAuth"><h1></h1></div>
+		</div>
+		<div class="footer">
+			<h1>Footer</h1></div>
+	</div>
+</body>
+</html>
