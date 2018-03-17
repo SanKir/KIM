3289459c4f9861715b79e990974dd54e7a6c5d94
ButovIvan <Super_address1998@outlook.com>
2018-03-17 23:57:41 +0700

    f

--- includes/header.php
+++ includes/header.php
@@ -18,15 +18,9 @@
 					<?php Input(); ?>
 			</div>
 			<div class="header">
-<<<<<<< HEAD
 				<a href="../index.php"><img id="logo" src="/image/logo3.png"></a>
 				<a href="../index.php"><img id="logo3" src="/image/fit.png"></a>
 				<a href="../index.php"><img id="logo2" src="/image/logo11.png"></a>
-=======
-				<a href="/index.php"><img id="logo" src="/image/logo3.png"></a>
-				<a href="/index.php"><img id="logo3" src="/image/fit.png"></a>
-				<a href="/index.php"><img id="logo2" src="/image/logo11.png"></a>
->>>>>>> 689853d9e7c70051337aaa6084f1cabc3b2d67d3
 			</div>
 			<div class="menu"></div>
 </div>
