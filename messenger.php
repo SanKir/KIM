03dce7d6d36a94ff9ff1267b5f90664b2acf25b1
ButovIvan <Super_address1998@outlook.com>
2018-03-21 21:19:50 +0700

    ff

---
+++ messenger.php
@@ -0,0 +1,43 @@
+<?php 
+	include "/includes/connection.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="style1.css">
+	<meta charset="UTF-8">
+	<title>Мои сообщения</title>
+</head>
+<body>
+	<div class="wrapper">
+		<?php include "/includes/header.php"; ?>
+		<div class="content54">
+		<a href="myprofile.php"><input type="submit" value="Моя страница" class="buttonMess"></a> <Br>
+		<input type="submit" value="Новости" class="buttonMess"><Br>
+		<input type="submit" value="Мои фотографии" class="buttonMess"><Br>
+		<a href="messenger.php"><input type="submit" value="Мои сообщения" class="buttonMess"></a><Br>
+		<input type="submit" value="Мои ответы" class="buttonMess"><Br>
+		<input type="submit" value="Мои Группы" class="buttonMess"><Br>
+		</div>
+		<div class="messenger"></div>
+		<div class="poleMess" >
+		<div class="divSpisok">
+		<pre class="preMess">Частые диалоги</pre>
+		<input type="submit"  value="Кирилл Саньков" id="button3Mess"></input>
+		<input type="submit"  value="Бутов Иван" id="button3Mess"></input>
+		<input type="submit"  value="Дудин Никита" id="button3Mess"></input>
+		<input type="submit"  value="Мамонт" id="button3Mess"></input>
+		</div>
+		<div class="divText"><input type="text"  class="textMess" readonly></div>
+		<div class="pole2Mess">
+		<textarea type="text" placeholder="Введите сообщение" id="text2Mess"></textarea>
+		<input type="text"  placeholder="Адрес получателя" id="text3Mess"></input>
+		<input type="submit"  value="Отправить" id="button2Mess"></input>
+		</div>
+		</div>
+		
+		<?php include "/includes/footer.php"; ?>
+	</div>
+</body>
+</html>
--- myprofile.php
+++ myprofile.php
@@ -12,7 +12,33 @@
 <body>
 	<div class="wrapper">
 		<?php include "/includes/header.php"; ?>
+		<div>
+		<div class="content54">
+		<a href="myprofile.php"><input type="submit" value="Моя страница" class="button56"></a> <Br>
+		<input type="submit" value="Новости" class="button56"><Br>
+		<input type="submit" value="Мои фотографии" class="button56"><Br>
+		<a href="messenger.php"><input type="submit" value="Мои сообщения" class="button56"></a><Br>
+		<input type="submit" value="Мои ответы" class="button56"><Br>
+		<input type="submit" value="Мои Группы" class="button56"><Br>
+		</div>
 		
+		<div class="content55">
+		<img src="/image/foto.jpg" class="foto">
+		<input type="submit" value="Изменить фотографию" class="button60"><Br>
+		</div>
+		<div class="content56">
+		<pre id="FIO">Бутов Иван                             Online</pre>
+		<pre id="Group">Дата Рождения: 10 июля 1998</pre>
+		<pre id="programs">Предпочитаемые языки программирования: С#, JS, HTMl,PHP,1C</pre> 
+		<input type="text" id="text" placeholder="Добавить запись на свою страницу"></input>
+		<input type="submit" value="Добавить" id="button101"></input>
+		<img src="/image/pic.png" id="pic"></img>
+		<img src="/image/foto.jpg" id="fotoP"></img>
+		<pre id="zapic">Жизнь удалась, если коньяк, который мы пьем,<Br> старше женщин, с которыми мы спим.<pre>
+		<pre id="zapic2">20.03.2018|<a href="#">Ответить</a><pre>
+		<a href="#"><img src="/image/like.png" id="Flike"></img></a>
+		</div>
+		</div>
 		<?php include "/includes/footer.php"; ?>
 	</div>
 </body>
