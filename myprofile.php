d0087db40875240981363408146aa52c38ff6f6c
IvanButov <Super_address1998@outlook.com>
2018-03-21 19:06:04 +0300

    132131

--- myprofile.php
+++ myprofile.php
@@ -1,5 +1,21 @@
 <?php 
 	include "/includes/connection.php";
+
+	function GetStatus()
+	{
+		$status = '';
+		if(isset($_SESSION['logged_user']))
+		{
+			$status = 'Online';
+		}
+		else $status = '';
+		return $status;
+	}
+
+?>
+
+<?php 
+	include "/includes/connection.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -12,32 +28,55 @@
 <body>
 	<div class="wrapper">
 		<?php include "/includes/header.php"; ?>
-		<div>
-		<div class="content54">
-		<a href="myprofile.php"><input type="submit" value="Моя страница" class="button56"></a> <Br>
-		<input type="submit" value="Новости" class="button56"><Br>
-		<input type="submit" value="Мои фотографии" class="button56"><Br>
-		<a href="messenger.php"><input type="submit" value="Мои сообщения" class="button56"></a><Br>
-		<input type="submit" value="Мои ответы" class="button56"><Br>
-		<input type="submit" value="Мои Группы" class="button56"><Br>
-		</div>
-		
-		<div class="content55">
-		<img src="/image/foto.jpg" class="foto">
-		<input type="submit" value="Изменить фотографию" class="button60"><Br>
-		</div>
-		<div class="content56">
-		<pre id="FIO">Бутов Иван                             Online</pre>
-		<pre id="Group">Дата Рождения: 10 июля 1998</pre>
-		<pre id="programs">Предпочитаемые языки программирования: С#, JS, HTMl,PHP,1C</pre> 
-		<input type="text" id="text" placeholder="Добавить запись на свою страницу"></input>
-		<input type="submit" value="Добавить" id="button101"></input>
-		<img src="/image/pic.png" id="pic"></img>
-		<img src="/image/foto.jpg" id="fotoP"></img>
-		<pre id="zapic">Жизнь удалась, если коньяк, который мы пьем,<Br> старше женщин, с которыми мы спим.<pre>
-		<pre id="zapic2">20.03.2018|<a href="#">Ответить</a><pre>
-		<a href="#"><img src="/image/like.png" id="Flike"></img></a>
-		</div>
+		<div class="my_profile clearfix">
+			<div class="profile_menu">
+				<ul>
+					<li><img src="/image/message.png"><a href="">Моя страница</a></li>
+					<li><img src="/image/message.png"><a href="/messenger.php">Мои сообщения</a></li>
+					<li><img src="/image/message.png"><a href="">Мои темы</a></li>
+				</ul>
+			</div>
+			<div class="photo">
+				<div class="myphoto">
+					<img src="/image/unnamed.png">
+				</div>
+				<div class="redact_photo">
+					<a href="">Редактировать</a>
+				</div>
+			</div>
+			<div class="my_info">
+				<div class="top_myinfo clearfix">
+					<div class="fio">
+						<p>Бутов Иван</p>
+					</div>
+					<div class="status">
+						<p><?php echo GetStatus(); ?></p>
+					</div>
+				</div>
+				<div class="main_info">
+					<p>
+						Дата рождения: 10 июня 1998<br>
+						Предпочитаемые языки программирования: С#, PHP, 1C<br>
+						<pre id="count_answ">Количество ответов: 100<br></pre>
+						<pre id="rep">Репутация: +100<br></pre>
+					</p>
+					<div class="rewards">
+						<h3>Мои награды:</h3>
+						<div class="my_rewards">
+							<img src="/image/sherif.jpg">
+							<img src="/image/sherif.jpg">
+							<img src="/image/sherif.jpg">
+							<img src="/image/sherif.jpg">
+						</div>
+					</div>
+				</div>
+				<div class="my_mind">
+				<form action="" method="post">
+					<textarea name="mind" placeholder="Добавить запись на свою страницу"></textarea>
+					<input type="submit" name="done">
+				</form>
+			</div>
+			</div>
 		</div>
 		<?php include "/includes/footer.php"; ?>
 	</div>
